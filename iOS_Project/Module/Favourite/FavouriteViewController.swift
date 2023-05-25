2e9c8ceea99a5cb0802a183232072f0850ab5b35
Asalah3 <asalahsayed3@gmail.com>
2023-05-25 19:32:10 +0300

    redesign Favorite

--- iOS_Project/Module/Favourite/FavouriteViewController.swift
+++ iOS_Project/Module/Favourite/FavouriteViewController.swift
@@ -27,10 +27,11 @@ extension FavouriteViewController : UITableViewDelegate , UITableViewDataSource{
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "FavouriteTableViewCell") as? FavouriteTableViewCell
         cell?.setUpCell()
+        cell?.layer.cornerRadius = 25
         return cell ?? UITableViewCell()
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 190
+        return 180
     }
 }

3b8aba204fc2b9a674e0dc59371ebf56b2c3f043
Soha Ahmed Hamdy <soha7hmed@gmail.com>
2023-05-26 12:44:35 +0300

    Home Design

--- Podfile
+++ Podfile
@@ -5,7 +5,7 @@ target 'iOS_Project' do
   # Comment the next line if you don't want to use dynamic frameworks
   use_frameworks!
 	pod 'SDWebImage', '~> 5.0'
-	pod 'Alamofire'
+	#pod 'Alamofire'
 	pod 'lottie-ios'
 
 
---
+++ iOS_Project/Module/Home/View/HomeViewController.swift
@@ -0,0 +1,48 @@
+//
+//  HomeViewController.swift
+//  iOS_Project
+//
+//  Created by Asalah Sayed on 25/05/2023.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController , UITableViewDelegate,UITableViewDataSource, UICollectionViewDelegate,UICollectionViewDataSource{
+    
+    @IBOutlet weak var categoryCollection: UICollectionView!
+    @IBOutlet weak var catergoryMeals: UITableView!
+    let caterNames = ["Popular","Breakfast","Lunch","Dinner","Dessert"]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Food Recipies"
+        
+
+        
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        0
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "item", for: indexPath)
+        
+    
+        return cell
+    }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        caterNames.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "item", for: indexPath) as! CategoriesCollectionViewCell
+        
+        cell.categoryName.text = caterNames[indexPath.row]
+        cell.catergoryImage.image = UIImage(named: caterNames[indexPath.row])
+    
+        return cell
+    }
+    
+    
+
+}

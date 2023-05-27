bf413acfca617076099faf7ee54c1008047cbd20
Asalah3 <asalahsayed3@gmail.com>
2023-05-27 10:57:45 +0300

    Added Dependency Injection in CoreDataTesr

--- iOS_ProjectTests/TestCoreData.swift
+++ iOS_ProjectTests/TestCoreData.swift
@@ -9,50 +9,51 @@ import XCTest
 @testable import iOS_Project
 final class TestCoreData: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    var favouriteItems: FavouriteProtocol?
+    override func setUp()  {
+        favouriteItems = FavouriteItems()
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    override func tearDown()  {
+        favouriteItems = nil
     }
     
     func testGetFavouritesResult(){
         // Given
-        FavouriteItems.favouriteItems.InsertItem(favouriteName: "name", favouriteId: 1, favouriteImage: "favouriteImage", favouriteMealCheif: "favouriteMealCheif", favouriteMealType: "favouriteMealType", favouriteServings: "favouriteServings")
+        favouriteItems?.InsertItem(favouriteName: "name", favouriteId: 1, favouriteImage: "favouriteImage", favouriteMealCheif: "favouriteMealCheif", favouriteMealType: "favouriteMealType", favouriteServings: "favouriteServings")
         // When
-        let result = FavouriteItems.favouriteItems.fetchFavouriteItems()
+        let result = favouriteItems?.fetchFavouriteItems()
         // Then
-        XCTAssertNotEqual(result.count, 0)
+        XCTAssertNotEqual(result?.count, 0)
 
     }
     
     func testInsertItem(){
         // Given
-        FavouriteItems.favouriteItems.InsertItem(favouriteName: "name", favouriteId: 1, favouriteImage: "favouriteImage", favouriteMealCheif: "favouriteMealCheif", favouriteMealType: "favouriteMealType", favouriteServings: "favouriteServings")
+        favouriteItems?.InsertItem(favouriteName: "name", favouriteId: 1, favouriteImage: "favouriteImage", favouriteMealCheif: "favouriteMealCheif", favouriteMealType: "favouriteMealType", favouriteServings: "favouriteServings")
         // When
-        let result = FavouriteItems.favouriteItems.fetchFavouriteItems()
+        let result = favouriteItems?.fetchFavouriteItems()
         // Then
-        XCTAssertNotEqual(result.count, 0)
+        XCTAssertNotEqual(result?.count, 0)
 
     }
     
     func testDeleteItemById(){
         // Given
-        FavouriteItems.favouriteItems.InsertItem(favouriteName: "name", favouriteId: 12, favouriteImage: "favouriteImage", favouriteMealCheif: "favouriteMealCheif", favouriteMealType: "favouriteMealType", favouriteServings: "favouriteServings")
-        FavouriteItems.favouriteItems.InsertItem(favouriteName: "name", favouriteId: 2, favouriteImage: "favouriteImage", favouriteMealCheif: "favouriteMealCheif", favouriteMealType: "favouriteMealType", favouriteServings: "favouriteServings")
-        FavouriteItems.favouriteItems.deleteItemById(favouriteId: 12)
+        favouriteItems?.InsertItem(favouriteName: "name", favouriteId: 12, favouriteImage: "favouriteImage", favouriteMealCheif: "favouriteMealCheif", favouriteMealType: "favouriteMealType", favouriteServings: "favouriteServings")
+        favouriteItems?.InsertItem(favouriteName: "name", favouriteId: 2, favouriteImage: "favouriteImage", favouriteMealCheif: "favouriteMealCheif", favouriteMealType: "favouriteMealType", favouriteServings: "favouriteServings")
+        favouriteItems?.deleteItemById(favouriteId: 12)
         // When
-        let result = FavouriteItems.favouriteItems.checkIfInserted(favouriteId: 12)
+        let result = favouriteItems?.checkIfInserted(favouriteId: 12)
         // Then
         XCTAssertNotEqual(result, true)
     }
     func testCheckIfInserted(){
         // Given
-        FavouriteItems.favouriteItems.InsertItem(favouriteName: "name", favouriteId: 1, favouriteImage: "favouriteImage", favouriteMealCheif: "favouriteMealCheif", favouriteMealType: "favouriteMealType", favouriteServings: "favouriteServings")
-        FavouriteItems.favouriteItems.InsertItem(favouriteName: "name", favouriteId: 2, favouriteImage: "favouriteImage", favouriteMealCheif: "favouriteMealCheif", favouriteMealType: "favouriteMealType", favouriteServings: "favouriteServings")
+        favouriteItems?.InsertItem(favouriteName: "name", favouriteId: 1, favouriteImage: "favouriteImage", favouriteMealCheif: "favouriteMealCheif", favouriteMealType: "favouriteMealType", favouriteServings: "favouriteServings")
+        favouriteItems?.InsertItem(favouriteName: "name", favouriteId: 2, favouriteImage: "favouriteImage", favouriteMealCheif: "favouriteMealCheif", favouriteMealType: "favouriteMealType", favouriteServings: "favouriteServings")
         // When
-        let result = FavouriteItems.favouriteItems.checkIfInserted(favouriteId: 1)
+        let result = favouriteItems?.checkIfInserted(favouriteId: 1)
         // Then
         XCTAssertEqual(result, true)
     }
